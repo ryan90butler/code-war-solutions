52a8765c83c9c898d44f474d2ad6c501ad732c0a
Ryan Butler <b6utle5r@gmail.com>
2018-09-24 17:17:20 -0600

    added solutions

--- Interview Practice Questions/split-decimal-return.js
+++ Interview Practice Questions/split-decimal-return.js
@@ -1,15 +1,18 @@
+
+
 function greaterPart(test){
   const newStr = String(test)
   const newNum = newStr.split('.')
   const num = newNum.map(function (x) {
-    return parseInt(x, 10);
-  });
-if(num[0]>num[1]){
-  return num[0]
+    return Number(x);
+    });
+  if(num[0]>num[1]){
+    return num[0]
+  }
+  if(num[1]>num[0]){
+    return num[1]
+  }else{
+    return test
+  }
 }
-if(num[1]>num[0]){
-  return num[1]
-}else{
-  return test
-}
-}
+console.log(greaterPart(43.79))
