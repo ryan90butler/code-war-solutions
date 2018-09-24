9aabc18ec534d84b0d789e5b3c3dbabda6881c64
Ryan Butler <b6utle5r@gmail.com>
2018-09-24 17:34:04 -0600

    fixed solution

--- Interview Practice Questions/split-decimal-return.js
+++ Interview Practice Questions/split-decimal-return.js
@@ -1,14 +1,14 @@
 
 
 function greaterPart(test){
-  const newStr = String(test)
-  return parseInt(newStr.split('.').sort((a,b)=>b-a)[0],10)
+  const newStr = String(test);
+  return parseInt(newStr.split('.').sort((a,b)=>b-a)[0],10);
 }
 console.log(greaterPart(100.211))
 
 
 function addArr(arr){
-  return arr.reduce((a,b)=>a+b,0)
+  return arr.reduce((a,b)=>a+b,0);
 }
 
 console.log(addArr([9,4]))
