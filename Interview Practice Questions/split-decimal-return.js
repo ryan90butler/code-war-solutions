746db70e5d9c7e632d0081e85496b0addef1e741
Ryan Butler <b6utle5r@gmail.com>
2018-09-24 18:07:08 -0600

    refactor

--- Interview Practice Questions/split-decimal-return.js
+++ Interview Practice Questions/split-decimal-return.js
@@ -1,14 +1,20 @@
-
+// function greaterPart(test){
+//   const newStr = String(test)
+//   const newNum = newStr.split('.')
+//   const num = newNum.map((x)=> parseInt(x, 10));
+//   if(num[0]>num[1]){
+//     return num[0]
+//   }
+//   if(num[1]>num[0]){
+//     return num[1]
+//   }else{
+//     return test
+//   }
+// }
 
 function greaterPart(test){
   const newStr = String(test);
-  return parseInt(newStr.split('.').sort((a,b)=>b-a)[0],10);
-}
-console.log(greaterPart(100.211))
-
-
-function addArr(arr){
-  return arr.reduce((a,b)=>a+b,0);
+  return Number(newStr.split('.').sort((a,b)=>b-a)[0]);
 }
 
-console.log(addArr([9,4]))
+console.log(greaterPart(32.109))
