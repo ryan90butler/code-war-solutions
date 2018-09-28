b426562d79de60b1e4f18822ae7b9da9cc7c6b34
Ryan Butler <b6utle5r@gmail.com>
2018-09-28 17:03:34 -0600

    added solution

---
+++ 8 kyu/create-the-rooms.js
@@ -0,0 +1,20 @@
+// You are creating an escape the room game. The first step is create an object called rooms that contains all of the rooms of the game. The room object should have at least 3 rooms as properties, and each individial room should have at least three properties (example: name, description, completed).
+
+// Add rooms here
+var rooms = {
+  bedroom: {
+    name: 'bedroom',
+    description: 'place to sleep',
+    completed: true
+  },
+  kitchen: {
+    name: 'kitchen',
+    description: 'place to eat',
+    completed: true
+  },
+  livingRoom: {
+    name: 'living room',
+    description: 'place to hang',
+    completed: true
+    }
+  }
